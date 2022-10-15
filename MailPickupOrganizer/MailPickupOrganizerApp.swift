018c56d50e16fca04faa6a82283b9000cd60b71a
MihirMishra23 <mihir.mishra@gmail.com>
2022-10-15 14:36:56 -0400

    project starter

---
+++ MailPickupOrganizer/MailPickupOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailPickupOrganizerApp.swift
+//  MailPickupOrganizer
+//
+//  Created by Mihir Mishra on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MailPickupOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
