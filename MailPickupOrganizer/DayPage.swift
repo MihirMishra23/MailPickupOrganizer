1384e7f8e8b5b145772c0fce97a59e4f0041203f
MihirMishra23 <mihir.mishra@gmail.com>
2022-10-16 23:50:17 -0400

    implemented filler page for time slot picker

---
+++ MailPickupOrganizer/DayPage.swift
@@ -0,0 +1,20 @@
+//
+//  DayPage.swift
+//  MailPickupOrganizer
+//
+//  Created by Mihir Mishra on 10/15/22.
+//
+
+import SwiftUI
+
+struct DayPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DayPage_Previews: PreviewProvider {
+    static var previews: some View {
+        DayPage()
+    }
+}
